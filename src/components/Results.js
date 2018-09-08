bf7868ce83d495b27663d4d92005c328edd4a4dc
darvid7 <david.anthony.lei@gmail.com>
2018-09-09 03:52:28 +1000

    Made results prettier

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import PacmanLoader from 'react-spinners/PacmanLoader';
+import BarLoader from 'react-spinners/BarLoader';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import ListItem from "./ListItem";
 
@@ -11,7 +11,8 @@ class Results extends Component {
     super(props);
     this.state = {
       loading: false,
-      searchResponseData: {}
+      searchResponseData: {},
+      queryStringShow: ""
     }
   }
 
@@ -19,18 +20,21 @@ class Results extends Component {
   // TODO: Call the url of the deployment and not local host?
   replaceMeLater = () => {
     this.setState({ loading: true });
+    // Find all patents for an org with these details.
+    const queryParams = {
+      uid: "my-user-id",
+      entityQueryType: "patent", // "patent" or "organisation".
+      queryName: "Google",  // Organisation name.
+      queryAcn: 999999999,  // 9 digit ACN.
+      queryAbn: 99999999999 // 11 digit ABN.
+    };
     axios.get("http://localhost:3000/search", {
-      // Find all patents for an org with these details.
       params: {
-        uid: "my-user-id",
-        entityQueryType: "patent", // "patent" or "organisation".
-        queryName: "Google",  // Organisation name.
-        queryAcn: 999999999,  // 9 digit ACN.
-        queryAbn: 99999999999 // 11 digit ABN.
+        ...queryParams
       }})
       .then((successResponse) => {
         // Parse response.
-        console.log("start")
+        console.log("start");
         let responseData = {};
         const searchResponses = successResponse.data;
         console.log(searchResponses);
@@ -44,7 +48,29 @@ class Results extends Component {
           }
         }
         console.log(responseData);
-        this.setState({ searchResponseData: responseData, loading: false })
+
+        const queryStringInfo = [];
+        if (queryParams.entityQueryType === "patent") {
+          queryStringInfo.push("show me patents for")
+        } else {
+          queryStringInfo.push("show me data for")
+        }
+        if (queryParams.queryName !== undefined && queryParams.queryName !== "") {
+          queryStringInfo.push("name: " + queryParams.queryName)
+        }
+
+        if (queryParams.queryAbn !== undefined && queryParams.queryAbn !== 0) {
+          queryStringInfo.push("ABN: " + queryParams.queryAbn)
+        }
+
+        if (queryParams.queryAcn !== undefined && queryParams.queryAcn !== "") {
+          queryStringInfo.push("ACN: " + queryParams.queryAcn)
+        }
+
+        const suffix = queryStringInfo.slice(1);
+        let queryStringShow = queryStringInfo[0] + " "  + suffix.join(" or ");
+
+        this.setState({ searchResponseData: responseData, loading: false, queryStringShow: queryStringShow })
       })
       .catch((error) => {
         this.setState({ loading: false });
@@ -59,16 +85,20 @@ class Results extends Component {
         <div
           style={{ textAlign: 'center'}}
           onClick={this.replaceMeLater}
-        >Get Results!
+        >
+          {this.state.queryStringShow.length > 0 ?
+            <div>Results for {this.state.queryStringShow}</div> :
+            <div>Get Results!</div>}
         </div>
         <div style={{ justifyContent: 'center', display: 'flex'}}>
-          <PacmanLoader
+          <BarLoader
             color={'#36D7B7'}
             size={50}
+            width={300}
             loading={this.state.loading}
           />
           {
-            Object.keys(this.state.searchResponseData).length > 0 ?
+            Object.keys(this.state.searchResponseData).length > 0 && this.state.loading === false ?
               <ListGroup>
                 {Object.keys(this.state.searchResponseData).map((k, i) =>
                   <ListItem key={i} dataObject={this.state.searchResponseData[k]}/>
